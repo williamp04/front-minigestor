179de4e8ee1581e47c4f9e1a3093927f3961c9c6
dortmund812 <carlosdavidgarcialopez@gmail.com>
2026-01-22 13:55:51 -0500

    main files

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import "react-quill/dist/quill.snow.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
